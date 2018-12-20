be76187b1308b2466e8a4eafbd69a04ce7b886ff
Alfie Whelan <alfie.whelan@nationwidedigital.co.uk>
2018-12-20 16:24:00 +0000

    add zipkin

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const pkg = require('./package.json')
+
+var appzip = require('appmetrics-zipkin')({
+    host: 'http://51.141.8.54',
+    port: 80,
+    serviceName:pkg.name,
+    sampleRate: 1.0
+  });
+
 const server = require('./lib/server')
 const PORT = process.env.PORT || 8001
-const pkg = require('./package.json')
-const log = require('./lib/log')
 
+const log = require('./lib/log')
 server.listen(PORT, () => {
     log().info(`${pkg.name} is running on ${PORT}`)
 })
